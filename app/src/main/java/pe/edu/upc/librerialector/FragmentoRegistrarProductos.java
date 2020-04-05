c2cef979f2256c5cd822eb47c6e12025d92c0eb0
rogerpuma <u201420675@upc.edu.pe>
2020-04-05 17:40:45 -0500

    Framentos Productos

--- app/src/main/java/pe/edu/upc/librerialector/FragmentoRegistrarProductos.java
+++ app/src/main/java/pe/edu/upc/librerialector/FragmentoRegistrarProductos.java
@@ -1,26 +1,20 @@
 package pe.edu.upc.librerialector;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
-import static pe.edu.upc.librerialector.R.id.listViewProductos;
 
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FragmentoListarProductos#newInstance} factory method to
+ * Use the {@link FragmentoRegistrarProductos#newInstance} factory method to
  * create an instance of this fragment.
- *
  */
-public class FragmentoListarProductos extends Fragment {
+public class FragmentoRegistrarProductos extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,25 +24,26 @@ public class FragmentoListarProductos extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    public FragmentoRegistrarProductos() {
+        // Required empty public constructor
+    }
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentoListarProductos.
+     * @return A new instance of fragment FragmentoRegistrarProductos.
      */
     // TODO: Rename and change types and number of parameters
-    public static FragmentoListarProductos newInstance(String param1, String param2) {
-        FragmentoListarProductos fragment = new FragmentoListarProductos();
+    public static FragmentoRegistrarProductos newInstance(String param1, String param2) {
+        FragmentoRegistrarProductos fragment = new FragmentoRegistrarProductos();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
-    public FragmentoListarProductos() {
-        // Required empty public constructor
     }
 
     @Override
@@ -58,17 +53,12 @@ public class FragmentoListarProductos extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
     }
-    private TextView tv1;
-    private ListView lv1;
 
-    private String descripciones [] = {"Lapicero","Borrador","Papel A4", "Tajador", "Plumones", "Colores"};
-    private String precios [] = {"10.00","11.00","12.00", "13.00", "14.00", "15.00"};
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragmento_listar_productos, container, false);
+        return inflater.inflate(R.layout.fragment_fragmento_registrar_productos, container, false);
     }
 }
--- app/src/main/java/pe/edu/upc/librerialector/MainActivity.java
+++ app/src/main/java/pe/edu/upc/librerialector/MainActivity.java
@@ -1,14 +1,55 @@
 package pe.edu.upc.librerialector;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private List<Fragment> fragments;
+    private FragmentTransaction transaction;
+    private Button btnListarProductos,btnRegistrarProductos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnListarProductos = (Button)findViewById(R.id.btnListarProductos);
+        btnRegistrarProductos = (Button)findViewById(R.id.btnRegistrarProductos);
+
+        btnListarProductos.setOnClickListener(this);
+        btnRegistrarProductos.setOnClickListener(this);
+
+        fragments = new ArrayList<>();
+
+        fragments.add(new FragmentoListarProductos()); //get(0)
+        fragments.add(new FragmentoRegistrarProductos()); //get(1)
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnListarProductos:
+                transaction = getSupportFragmentManager().beginTransaction();
+                //transaction.replace(R.id.container, fragments.get(1));
+                transaction.replace(R.id.containerProductos,fragments.get(0));
+                transaction.commit();
+
+                break;
+            case R.id.btnRegistrarProductos:
+                transaction = getSupportFragmentManager().beginTransaction();
+                //transaction.replace(R.id.container, fragments.get(1));
+                transaction.replace(R.id.containerProductos,fragments.get(1));
+                transaction.commit();
+                break;
+        }//fin de switch
     }
 }
