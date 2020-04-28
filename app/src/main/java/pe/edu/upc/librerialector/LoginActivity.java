ffbbc6cd4c0c2850b90824de03698aaed6d44755
rogerpuma <u201420675@upc.edu.pe>
2020-04-27 19:28:03 -0500

    modificar manifest

--- app/src/main/java/pe/edu/upc/librerialector/LoginActivity.java
+++ app/src/main/java/pe/edu/upc/librerialector/LoginActivity.java
@@ -13,7 +13,7 @@ import android.widget.Toast;
 public class LoginActivity extends AppCompatActivity {
     String user, clave;
     EditText edtuser, edtpass;
-    Button btnlogin;
+    Button btnlogin,btnregistrar;
     Context contexto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +26,8 @@ public class LoginActivity extends AppCompatActivity {
         user=edtuser.getText().toString();
         clave= edtpass.getText().toString();
 
+        btnregistrar = findViewById(R.id.btnregistrar);
+
         btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,7 +36,39 @@ public class LoginActivity extends AppCompatActivity {
                     startActivity(Listar);
                 }
                 else Toast.makeText(contexto, "Usuario o clave incorrecta", Toast.LENGTH_SHORT).show();
+                //Inicio
+                /*final String username = edtuser.getText().toString();
+                final String password = edtpass.getText().toString();
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if(success){
+                                Intent intent = new Intent(LoginActivity.this,ListarActivity.class);
+                                LoginActivity.this.startActivity(intent);
+                            }else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage("error registro").setNegativeButton("Retry",null).create().show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(username,password,responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);*/
+                //Fin
+            }
+        });
 
+        btnregistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registro = new Intent(LoginActivity.this, RegistrarUsuario.class);
+                startActivity(registro);
             }
         });
     }
